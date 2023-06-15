35b0c8c38c6770560a91591ccc40ef84fb563cec
Jay <jayprajapati7364@gmail.com>
2023-06-15 11:35:16 +0530

    Updated Config.js file

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_xoi54Q7A1', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '51rd5549pklipn5h3abt6fgkts', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_hf3QxE9hP', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2alv8t0q6v6er8j28tq62h2q81', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
